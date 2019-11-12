2bf3c9a698924cb16d369badaceaab31026852a2
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 20:58:56 -0800

    edit add feature and delete feature reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,60 @@
-import {ADD_FEATURE, DELETE_FEATURE} from '../actions';
+import { ADD_FEATURE, DELETE_FEATURE } from "../actions";
 
 const initialState = {
   additionalPrice: 0,
   car: {
     price: 26395,
-    name: '2019 Ford Mustang',
+    name: "2019 Ford Mustang",
     image:
-      'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
     features: []
   },
   additionalFeatures: [
-    { id: 1, name: 'V-6 engine', price: 1500 },
-    { id: 2, name: 'Racing detail package', price: 1500 },
-    { id: 3, name: 'Premium sound system', price: 500 },
-    { id: 4, name: 'Rear spoiler', price: 250 }
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 }
   ]
 };
 
+const filter = (list, id) => {
+  const [item] = list.filter(({ id: itemID }) => itemID === id);
+  const others = list.filter(({ id: itemID }) => itemID !== id);
+  return [item, others];
+};
+
+const addFeature = (list, item) => [...list, item];
+
 export const reducer = (state = initialState, action) => {
-  switch (action.type){
-    case ADD_FEATURE: 
-      return;
-    case DELETE_FEATURE:
-      return;
+  const { car: carObj } = state;
+  switch (action.type) {
+    case ADD_FEATURE: {
+      const { id: payloadID } = action.payload;
+      const { additionalFeatures: featureToFilter } = state;
+      const [feature, additionalFeatures] = filter(featureToFilter, payloadID);
+
+      const features = addFeature(carObj.features, feature);
+      const car = { ...carObj, features };
+      const additionalPrice = state.additionalPrice + feature.price;
+
+      const newState = { ...state, car, additionalFeatures, additionalPrice };
+      return newState;
+    }
+    case DELETE_FEATURE: {
+      const { id: payloadID } = action.payload;
+      const { features: featuresToFilter } = carObj;
+      const [feature, carFeatures] = filter(featuresToFilter, payloadID);
+
+
+      const car = { ...carObj, features: carFeatures };
+      const additionalFeatures = addFeature(state.additionalFeatures, feature);
+      const additionalPrice = state.additionalPrice - feature.price;
+
+      const newState = { ...state, car, additionalFeatures, additionalPrice };
+      return newState;
+    }
     default:
-      return initialState;
+      console.log("We here");
+      return state;
   }
-}
+};
