eb744b752ccf692938bec3e80f26204c1dcebde1
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 19:50:35 -0800

    connect redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 const Header = props => {
   return (
@@ -12,4 +13,10 @@ const Header = props => {
   );
 };
 
-export default Header;
+const mapStateToProps = ({ car }) => {
+  return {
+    car: { ...car }
+  };
+};
+
+export default connect(mapStateToProps, {})(Header);
