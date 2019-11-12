6645d4ff236b08e2a45a97a61bdc54caef033142
Victor Tran <victor.n.tran@outlook.com>
2019-11-11 20:54:31 -0800

    import and export function creators and action types

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+import { addFeature, deleteFeature } from "./creators";
+import { ADD_FEATURE, DELETE_FEATURE } from "./actions";
+
+export { addFeature, deleteFeature, ADD_FEATURE, DELETE_FEATURE };
